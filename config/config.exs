dd2792b053a4c8adba3668b4347e09c37cb5e3a0
Farsheed Ashouri <rodmena@me.com>
2019-08-14 20:40:49 +0430

    added config file

---
+++ config/config.exs
@@ -0,0 +1,7 @@
+use Mix.Config
+
+config :mail_in_a_box,
+  admin_user: "guess@me",
+  admin_pass: "39uq8k7",
+  endpoint: "https://box.me.net",
+  domain: "me.net"
--- test/mail_in_a_box_test.exs
+++ test/mail_in_a_box_test.exs
@@ -2,7 +2,39 @@ defmodule MailInABoxTest do
   use ExUnit.Case
   doctest MailInABox
 
-  test "greets the world" do
-    assert MailInABox.hello() == :world
+  @emailuser Application.fetch_env!(:mail_in_a_box, :admin_user)
+  @domain Application.fetch_env!(:mail_in_a_box, :domain)
+
+  test "get available email accounts" do
+    result =
+      MailInABox.get_accounts()
+      |> Enum.filter(fn x -> x["email"] == @emailuser end)
+
+    assert length(result) == 1
+  end
+
+  test "adding/removing email accounts" do
+    targetmail = "something@#{@domain}"
+    targetpass = "secretisgood"
+
+    assert MailInABox.add_account(targetmail, targetpass) == {:ok, 200}
+
+    chckfn = fn ->
+      MailInABox.get_accounts()
+      |> Enum.filter(fn x -> x["email"] == targetmail end)
+    end
+
+    result = chckfn.()
+    assert length(result) == 1
+
+    assert MailInABox.remove_account(targetmail) == {:ok, 200}
+  end
+
+  test "handle errors" do
+    targetmail = "123"
+    targetpass = "secretisgood"
+
+    {:error, 400, desc} = MailInABox.add_account(targetmail, targetpass)
+    assert desc =~ "Invalid"
   end
 end
