80d2a28e849cd5f57329ce8f6e341902c96c4fc9
Farsheed Ashouri <rodmena@me.com>
2019-08-15 20:51:11 +0430

    added more modules

--- config/config.exs
+++ config/config.exs
@@ -1,7 +1,37 @@
 use Mix.Config
 
-config :mail_in_a_box,
-  admin_user: "guess@me",
-  admin_pass: "39uq8k7",
-  endpoint: "https://box.me.net",
-  domain: "me.net"
+config :mail_in_a_box, MIAB.Admin,
+  admin_user: "guess@example.org",
+  admin_pass: "39uq8k7SW8",
+  endpoint: "https://box.example.org",
+  domain: "example.org"
+
+config :mail_in_a_box, MIAB.IMAP,
+  account: "noreply@example.org",
+  password: "14de0ea2-6187",
+  use_ssl: false,
+  incoming_mail_server: "box.example.org",
+  incoming_port: 993
+
+config :mail_in_a_box, MIAB.Sender,
+  adapter: Swoosh.Adapters.SMTP,
+  relay: "box.example.org",
+  username: "noreply@example.org",
+  password: "bbe8-33690cfbfeff",
+  ssl: false,
+  tls: :always,
+  auth: :always,
+  port: 587,
+  no_mx_lookups: false,
+  retries: 2
+
+config :mail_in_a_box, MIAB.Send,
+  ## Sender information
+  email: "noreply@example.org",
+  from: "Iranian English Learning Platform",
+  org: "Iranian English Learning Mock Tests Center",
+  org_email: "info@example.org",
+  org_webiste: "https://example.org",
+  # user email will be appended to unsub url
+  unsub_url: "https://pte.example.org/api/v1/emails/unsubscribe/",
+  signature: "\nRegards,\nYour English Learning Platform Team."
--- mix.exs
+++ mix.exs
@@ -18,7 +18,7 @@ defmodule MailInABox.MixProject do
     # These are the default files included in the package
     [
       name: :mail_in_a_box,
-      files: ["lib", "mix.exs", "README*", "config", "test"],
+      files: ["lib", "mix.exs", "README*", "config", "test", "priv/*.eex"],
       maintainers: ["Farsheed Ashouri"],
       licenses: ["MIT License"],
       links: %{"GitHub" => "https://github.com/ourway/mail_in_a_box"}
@@ -37,7 +37,14 @@ defmodule MailInABox.MixProject do
   defp deps do
     [
       {:jason, ">= 0.0.0"},
+      # For sending emails:
+      {:swoosh, "~> 0.23"},
+      {:gen_smtp, "~> 0.13"},
+      # For Imap commands
+      {:eximap, "~> 0.1.1-dev"},
+      # test and development,
       {:credo, "~> 1.0", only: [:dev, :test], runtime: false},
+      # docs
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
